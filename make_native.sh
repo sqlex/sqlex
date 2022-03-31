c276cb0319b728fd13c4432e1d1b0f0a647bb4c9
李雨田 <demon@danwi.me>
2022-03-31 15:49:26 +0800

    添加构建原生库的脚本

---
+++ make_native.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# golang 版本
+go version
+
+if [[ "$OSTYPE" =~ ^darwin ]];then
+    LIB="libsqlex.dylib"
+    if [[ $(uname -m) == 'arm64' ]]; then
+      DIST="../../parser/src/main/resource/native/darwin/aarch64"
+    else
+      DIST="../../parser/src/main/resource/native/darwin/x86"
+    fi 
+elif [[ "$OSTYPE" =~ ^linux ]]; then
+    LIB="libsqlex.dylib"
+    DIST="../../parser/src/main/resource/native/linux"
+else
+  echo "不支持的操作系统,目前仅支持macos/linux"
+  exit 1
+fi
+
+echo "构建原生库"
+#构建原生库
+cd 3rdparty/mysql && go build -buildmode c-shared -o $DIST/$LIB github.com/pingcap/tidb/sqlex
+
