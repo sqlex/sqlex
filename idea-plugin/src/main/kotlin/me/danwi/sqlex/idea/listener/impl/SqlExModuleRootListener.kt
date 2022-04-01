54d3d30a42f4f071637a9e084c1165f2bf50317b
李雨田 <demon@danwi.me>
2022-04-01 14:02:20 +0800

    自动去掉标记为源码目录的自动生成目录

--- idea-plugin/src/main/kotlin/me/danwi/sqlex/idea/listener/impl/SqlExModuleRootListener.kt
+++ idea-plugin/src/main/kotlin/me/danwi/sqlex/idea/listener/impl/SqlExModuleRootListener.kt
@@ -3,9 +3,12 @@ package me.danwi.sqlex.idea.listener.impl
 import com.intellij.openapi.roots.ModuleRootEvent
 import com.intellij.openapi.roots.ModuleRootListener
 import me.danwi.sqlex.idea.service.syncRepositoryService
+import me.danwi.sqlex.idea.util.extension.modules
+import me.danwi.sqlex.idea.util.extension.unmarkAllGeneratedSourceRoot
 
 class SqlExModuleRootListener : ModuleRootListener {
     override fun rootsChanged(event: ModuleRootEvent) {
+        event.project.modules.forEach { it.unmarkAllGeneratedSourceRoot() }
         syncRepositoryService(event.project)
     }
 }
