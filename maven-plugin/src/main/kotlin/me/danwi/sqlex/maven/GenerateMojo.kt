be599c2aff52716745a06313ae983e92a78f2ba0
李雨田 <demon@danwi.me>
2022-04-07 22:50:35 +0800

    添加默认sqlex repository位置

--- maven-plugin/src/main/kotlin/me/danwi/sqlex/maven/GenerateMojo.kt
+++ maven-plugin/src/main/kotlin/me/danwi/sqlex/maven/GenerateMojo.kt
@@ -8,6 +8,7 @@ import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.Parameter
 import org.apache.maven.project.MavenProject
 import java.io.File
+import java.nio.file.Paths
 
 @Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 class GenerateMojo : AbstractMojo() {
@@ -15,16 +16,16 @@ class GenerateMojo : AbstractMojo() {
     private var project: MavenProject? = null
 
     /**
-     * SqlEx Repository目录
+     * SqlEx Repository目录(默认为src/main/sqlex)
      */
     @Parameter
-    private var sources: Array<File> = arrayOf()
+    private var sources: Array<File>? = null
 
     /**
-     * 测试SqlEx Repository目录
+     * 测试SqlEx Repository目录(默认为src/test/sqlex)
      */
     @Parameter
-    private var testSources: Array<File> = arrayOf()
+    private var testSources: Array<File>? = null
 
     /**
      * 生成源码输出目录(通常不用自己配置)
@@ -56,16 +57,22 @@ class GenerateMojo : AbstractMojo() {
     }
 
     override fun execute() {
+        val project = this.project ?: throw Exception("无法获取任务所在项目")
+
+        val sourceRoots =
+            this.sources ?: arrayOf(Paths.get(project.basedir.absolutePath, "src", "main", "sqlex").toFile())
         val outputPath = outputPath
         if (outputPath != null) {
-            generateSourceTo(sources, outputPath)
-            project?.addCompileSourceRoot(outputPath.absolutePath)
+            generateSourceTo(sourceRoots, outputPath)
+            project.addCompileSourceRoot(outputPath.absolutePath)
         }
 
+        val testSourceRoots =
+            this.sources ?: arrayOf(Paths.get(project.basedir.absolutePath, "src", "test", "sqlex").toFile())
         val testOutputPath = testOutputPath
         if (testOutputPath != null) {
-            generateSourceTo(testSources, testOutputPath)
-            project?.addTestCompileSourceRoot(testOutputPath.absolutePath)
+            generateSourceTo(testSourceRoots, testOutputPath)
+            project.addTestCompileSourceRoot(testOutputPath.absolutePath)
         }
     }
 }
