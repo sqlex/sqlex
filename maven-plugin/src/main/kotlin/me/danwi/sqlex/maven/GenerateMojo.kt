49eb622c69ed2d9784f7489a3ffaa8d165abe7dc
李雨田 <demon@danwi.me>
2022-04-07 22:14:49 +0800

    优化maven插件,删除add-source任务,添加test源码的支持

--- maven-plugin/src/main/kotlin/me/danwi/sqlex/maven/GenerateMojo.kt
+++ maven-plugin/src/main/kotlin/me/danwi/sqlex/maven/GenerateMojo.kt
@@ -9,25 +9,43 @@ import org.apache.maven.plugins.annotations.Parameter
 import org.apache.maven.project.MavenProject
 import java.io.File
 
-@Mojo(name = "generate", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 class GenerateMojo : AbstractMojo() {
     @Parameter(readonly = true, defaultValue = "\${project}")
     private var project: MavenProject? = null
 
+    /**
+     * SqlEx Repository目录
+     */
+    @Parameter
+    private var sources: Array<File> = arrayOf()
+
+    /**
+     * 测试SqlEx Repository目录
+     */
+    @Parameter
+    private var testSources: Array<File> = arrayOf()
+
+    /**
+     * 生成源码输出目录(通常不用自己配置)
+     */
     @Parameter(defaultValue = "\${project.build.directory}/sqlex")
     private var outputPath: File? = null
 
-    override fun execute() {
-        val outputPath = this.outputPath ?: throw Exception("输出目录未指定")
+    /**
+     * 生成测试源码输出目录(通常不用自己配置)
+     */
+    @Parameter(defaultValue = "\${project.build.directory}/sqlex-test")
+    private var testOutputPath: File? = null
 
+    private fun generateSourceTo(sourceRoots: Array<File>, outputPath: File) {
         //获取所有的sqlex source roots
-        val sourceRoots = project?.compileSourceRoots
-            ?.map { File(it) }
-            ?.filter { it.exists() && it.isDirectory }
-            ?.filter {
+        sourceRoots
+            .filter { it.exists() && it.isDirectory }
+            .filter {
                 val configFile = File(it.absolutePath, SqlExConfigFileName)
                 configFile.exists() && configFile.isFile
-            } ?: listOf()
+            }
         //删除旧的源码
         outputPath.deleteRecursively()
         //生成源码
@@ -35,8 +53,19 @@ class GenerateMojo : AbstractMojo() {
             log.info("generate [${it.path}] to [${outputPath.path ?: "UNKNOWN"}]")
             generateRepositorySource(it, outputPath)
         }
+    }
 
-        //添加生成的源码目录到源码集合
-        project?.addCompileSourceRoot(this.outputPath?.absolutePath ?: return)
+    override fun execute() {
+        val outputPath = outputPath
+        if (outputPath != null) {
+            generateSourceTo(sources, outputPath)
+            project?.addCompileSourceRoot(outputPath.absolutePath)
+        }
+
+        val testOutputPath = testOutputPath
+        if (testOutputPath != null) {
+            generateSourceTo(testSources, testOutputPath)
+            project?.addTestCompileSourceRoot(testOutputPath.absolutePath)
+        }
     }
 }
