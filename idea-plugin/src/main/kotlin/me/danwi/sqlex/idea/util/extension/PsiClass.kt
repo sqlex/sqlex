809dd4b74a4083a07c8b46f8ab0f56aa266edede
李雨田 <demon@danwi.me>
2022-04-08 11:05:26 +0800

    修复PsiClass获取全限定报名错误的问题,该问题导致无法取得特定包下的类(破坏了智能提示)

--- idea-plugin/src/main/kotlin/me/danwi/sqlex/idea/util/extension/PsiClass.kt
+++ idea-plugin/src/main/kotlin/me/danwi/sqlex/idea/util/extension/PsiClass.kt
@@ -2,6 +2,6 @@ package me.danwi.sqlex.idea.util.extension
 
 import com.intellij.psi.PsiClass
 
-//获取权限定包名
+//获取全限定包名
 val PsiClass.qualifiedPackageName: String?
-    inline get() = this.qualifiedName?.removePrefix(".${this.name}")
+    inline get() = this.qualifiedName?.removeSuffix(".${this.name}")
