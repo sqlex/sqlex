31adc826e9de8efc3c82e7ac16f750bee2daba84
李雨田 <demon@danwi.me>
2022-04-10 13:52:35 +0800

    给MethodSubtree创建ItemPresentation

--- idea-plugin/src/main/kotlin/me/danwi/sqlex/idea/sqlm/psi/MethodSubtree.kt
+++ idea-plugin/src/main/kotlin/me/danwi/sqlex/idea/sqlm/psi/MethodSubtree.kt
@@ -1,8 +1,14 @@
 package me.danwi.sqlex.idea.sqlm.psi
 
+import com.intellij.icons.AllIcons
 import com.intellij.lang.ASTNode
+import com.intellij.navigation.ItemPresentation
+import com.intellij.psi.PsiMethod
 import com.intellij.psi.tree.IElementType
+import me.danwi.sqlex.idea.service.sqlexMethodPsiClassCacheKey
+import me.danwi.sqlex.idea.util.extension.childrenOf
 import org.antlr.intellij.adaptor.psi.IdentifierDefSubtree
+import javax.swing.Icon
 
 class MethodSubtree(node: ASTNode, idElementType: IElementType) : IdentifierDefSubtree(node, idElementType) {
     val returnType: ReturnTypeSubtree?
@@ -16,4 +22,31 @@ class MethodSubtree(node: ASTNode, idElementType: IElementType) : IdentifierDefS
 
     val sql: SqlSubtree?
         get() = children.find { it is SqlSubtree } as SqlSubtree?
+
+    val javaMethod: PsiMethod?
+        get() {
+            val javaClass = this.containingFile.virtualFile.getUserData(sqlexMethodPsiClassCacheKey) ?: return null
+            val methodName = this.methodName?.methodName ?: return null
+            return javaClass.childrenOf<PsiMethod>().find { it.name == methodName }
+        }
+
+    override fun getPresentation(): ItemPresentation {
+        return MethodItemPresentation(this)
+    }
+}
+
+class MethodItemPresentation(private val element: MethodSubtree) : ItemPresentation {
+    override fun getLocationString(): String {
+        return element.containingFile.virtualFile.path
+    }
+
+    override fun getPresentableText(): String {
+        val methodName = element.methodName?.methodName ?: "未命名方法"
+        val params = element.paramList?.params ?: listOf()
+        return "${methodName}(${params.joinToString(", ") { "${it.paramName?.text ?: "_"}:${it.paramType?.text ?: "_"}${if (it.isCollectionParam) "*" else ""}" }})"
+    }
+
+    override fun getIcon(unused: Boolean): Icon {
+        return AllIcons.Providers.Mysql
+    }
 }
