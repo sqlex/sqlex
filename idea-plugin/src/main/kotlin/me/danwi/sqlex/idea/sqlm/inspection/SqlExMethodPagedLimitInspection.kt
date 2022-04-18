309093c2a8de3ef00c879a0b97860a19d083008d
戴凯杰 <xpress@danwi.me>
2022-04-18 17:49:53 +0800

    优化limit 1的判断

--- idea-plugin/src/main/kotlin/me/danwi/sqlex/idea/sqlm/inspection/SqlExMethodPagedLimitInspection.kt
+++ idea-plugin/src/main/kotlin/me/danwi/sqlex/idea/sqlm/inspection/SqlExMethodPagedLimitInspection.kt
@@ -21,15 +21,27 @@ class SqlExMethodPagedLimitInspection : LocalInspectionTool() {
                 if (element !is SqlLimitClause) return
                 //判断是否在子句中
                 if (element.parentOf<SqlParenthesizedExpression>() != null) return
-                //判断是否有offset
-                val hasOffset = element.children.any { it.text.lowercase() == "offset" }
-                //判断是否是limit 1
-                val count1 = if (hasOffset) element.childrenOf<SqlLiteralExpression>().firstOrNull()?.text == "1"
-                else element.childrenOf<SqlLiteralExpression>().lastOrNull()?.text == "1"
                 //获取分页方法
                 element.containingSqlExMethod?.childrenOf<PagedSubtree>()?.firstOrNull() ?: return
 
-                if (count1) {
+                //判断是否有offset
+                val hasOffset = element.children.any { it.text.lowercase() == "offset" }
+                //判断是否有,
+                val hasComma = element.children.any { it.text == "," }
+                //获取数字常量
+                val nums = element.children.filterIsInstance<SqlLiteralExpression>().map { it.text }
+                //limit count文本
+                val countText = if (hasOffset && nums.size == 2) { //存在offset且数字个数为2
+                    nums[0]
+                } else if (hasComma && nums.size == 2) { //存在,且数字个数为2
+                    nums[1]
+                } else if (nums.size == 1) { //数字个数为1
+                    nums[0]
+                } else { //其他情况, 直接返回
+                    return
+                }
+                //判断是否是limit 1
+                if (countText == "1") {
                     holder.registerProblem(element, "使用分页方法时不能使用Limit 1")
                 } else {
                     holder.registerProblem(element, "正在分页方法内使用Limit N", ProblemHighlightType.WEAK_WARNING)
