5d34c46f32420e848806da6dc81a5f9e04dad84e
棒棒大 <demon@danwi.me>
2022-04-18 18:11:27 +0800

    重构java代码生成器 (#58)
    * 给SqlExMarkerPosition和SqlExParameterPosition注解添加默认值
    
    * 重构java代码生成器,采用javapoet来生成代码
    
    * 调整重名参数判断的位置

--- core/src/main/java/me/danwi/sqlex/core/annotation/SqlExMarkerPosition.java
+++ core/src/main/java/me/danwi/sqlex/core/annotation/SqlExMarkerPosition.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SqlExMarkerPosition {
-    int[] value();
+    int[] value() default {};
 }
---
+++ parser/src/main/kotlin/me/danwi/sqlex/parser/generate/GeneratedJavaFile.kt
@@ -0,0 +1,71 @@
+package me.danwi.sqlex.parser.generate
+
+import com.squareup.javapoet.AnnotationSpec
+import com.squareup.javapoet.ClassName
+import com.squareup.javapoet.JavaFile
+import com.squareup.javapoet.MethodSpec
+import com.squareup.javapoet.TypeName
+import com.squareup.javapoet.TypeSpec
+import me.danwi.sqlex.parser.exception.SqlExRepositoryMethodException
+import me.danwi.sqlex.parser.util.packageNameToRelativePath
+import me.danwi.sqlex.parser.util.pascalName
+import javax.lang.model.element.Modifier
+
+val CorePackageName = PackageName("me.danwi.sqlex.core")
+val CoreAnnotationsPackageName = PackageName("me.danwi.sqlex.core.annotation")
+val CoreTypesPackageName = PackageName("me.danwi.sqlex.core.type")
+
+class PackageName(private val packageName: String) {
+    fun getClassName(className: String): ClassName {
+        return ClassName.get(packageName, className)
+    }
+}
+
+abstract class GeneratedJavaFile(val packageName: String, val className: String) {
+    val qualifiedName: String
+        get() = "$packageName.$className"
+
+    val relativePath: String
+        get() = "${packageName.packageNameToRelativePath}/${className}.java"
+
+    //生成的源码
+    val source: String by lazy {
+        try {
+            val type = this.generate().toBuilder().addAnnotation(
+                AnnotationSpec
+                    .builder(CoreAnnotationsPackageName.getClassName("SqlExGenerated"))
+                    .build()
+            ).build()
+            val source = JavaFile.builder(packageName, type).indent("    ").build().toString()
+            source.replace("${RepositoryPackageNamePlaceHolder}.", "")
+        } catch (e: Exception) {
+            throw SqlExRepositoryMethodException(
+                relativePath,
+                e.message ?: "未知的Method解析错误(${e::class.java.simpleName})"
+            )
+        }
+    }
+
+    //具体的生成方法
+    protected abstract fun generate(): TypeSpec
+}
+
+//给类添加getter/setter
+fun TypeSpec.Builder.addGetterAndSetter(name: String, type: TypeName): TypeSpec.Builder {
+    this.addField(type, "_${name}", Modifier.PRIVATE)
+    this.addMethod(
+        MethodSpec.methodBuilder("get${name.pascalName}")
+            .addModifiers(Modifier.PUBLIC)
+            .addStatement("return this._${name}")
+            .returns(type)
+            .build()
+    )
+    this.addMethod(
+        MethodSpec.methodBuilder("set${name.pascalName}")
+            .addModifiers(Modifier.PUBLIC)
+            .addParameter(type, "value")
+            .addStatement("this._${name} = value")
+            .build()
+    )
+    return this
+}
