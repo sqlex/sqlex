d39735de8e1deb6fff4175db1141ad616e24e36b
戴凯杰 <xpress@danwi.me>
2022-04-29 15:53:42 +0800

    调整比对逻辑

--- core/src/main/java/me/danwi/sqlex/core/checker/Checker.java
+++ core/src/main/java/me/danwi/sqlex/core/checker/Checker.java
@@ -9,8 +9,9 @@ import org.slf4j.LoggerFactory;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
 
 public class Checker {
     private final Logger logger = LoggerFactory.getLogger(getClass());
@@ -22,74 +23,61 @@ public class Checker {
     }
 
     public void check() {
-        logger.info("获取repo表结构");
-        List<TableInfo> annotationTables = new ArrayList<>();
-        for (SqlExTableInfo t : this.factory.getRepositoryClass().getAnnotationsByType(SqlExTableInfo.class)) {
-            List<ColumnInfo> columns = new ArrayList<>();
-            for (int i = 0; i < t.columnNames().length; i++) {
-                columns.add(new ColumnInfo(
-                        t.columnNames()[i],
-                        JDBCType.valueOf(t.columnTypeIds()[i]),
-                        t.columnTypeNames()[i],
-                        t.columnLengths()[i],
-                        t.columnUnsigneds()[i]
-                ));
-            }
-            annotationTables.add(new TableInfo(t.name(), columns));
-        }
+        logger.info("获取({})表结构", factory.getRepositoryClass().getPackage().getName());
+        List<TableInfo> repositoryTables = getRepositoryTables();
         logger.info("获取数据库表结构");
         List<TableInfo> databaseTables = getDatabaseTables();
         logger.info("开始比对");
-        List<TableInfo> diffTables = diff(annotationTables, databaseTables);
+        List<TableInfo> diffTables = diff(repositoryTables, databaseTables);
         if (diffTables.size() != 0) {
             throw new SqlExCheckException(diffTables);
         }
         logger.info("比对完成");
     }
 
-    /**
-     * 表信息比对
-     *
-     * @param sourceTables 源
-     * @param targetTables 目标
-     * @return 源 - 目标
-     */
-    private List<TableInfo> diff(List<TableInfo> sourceTables, List<TableInfo> targetTables) {
+    private List<TableInfo> diff(List<TableInfo> source, List<TableInfo> target) {
+        Map<String, Map<String, ColumnInfo>> sourceTables = toTableColumnMap(source);
+        Map<String, Map<String, ColumnInfo>> targetTables = toTableColumnMap(target);
+
         List<TableInfo> diffTables = new ArrayList<>();
-        sourceTable:
-        for (TableInfo source : sourceTables) {
-            for (TableInfo target : targetTables) {
-                if (Objects.equals(source.name, target.name)) {
-                    List<ColumnInfo> diffColumns = new ArrayList<>();
-                    for (ColumnInfo sc : source.columns) {
-                        for (ColumnInfo tc : target.columns) {
-                            if (Objects.equals(sc.name, tc.name)) {
-                                //repository的tinyint(1) 等同于 数据库的bit(1)
-                                if (sc.typeId == JDBCType.TINYINT && sc.length == 1 && tc.typeId == JDBCType.BIT && tc.length == 1)
-                                    continue;
-                                if (!(sc.typeId == tc.typeId) || MysqlType.isSigned(MysqlType.getByJdbcType(sc.typeId.getVendorTypeNumber())) && sc.unsigned != tc.unsigned) {
-                                    diffColumns.add(sc);
-                                }
-                            }
-                        }
+        sourceTables.forEach((sourceTableName, sourceColumnMap) -> {
+            Map<String, ColumnInfo> targetColumnMap = targetTables.get(sourceTableName);
+            if (targetColumnMap != null) { //数据库表存在, 比对列信息
+                List<ColumnInfo> diffColumns = new ArrayList<>();
+                sourceColumnMap.forEach((sourceColumnName, sourceColumn) -> {
+                    ColumnInfo targetColumn = targetColumnMap.get(sourceColumnName);
+                    //repository的tinyint(1) 等同于 数据库的bit(1)
+                    if (targetColumn != null && sourceColumn.typeId == JDBCType.TINYINT && sourceColumn.length == 1 && targetColumn.typeId == JDBCType.BIT && targetColumn.length == 1) {
+                        return;
                     }
-                    if (diffColumns.size() > 0) {
-                        diffTables.add(new TableInfo(source.name, diffColumns));
+                    if (targetColumn == null || sourceColumn.typeId != targetColumn.typeId
+                    ) {
+                        diffColumns.add(sourceColumn);
                     }
-                    continue sourceTable;
+                });
+                if (diffColumns.size() > 0) {
+                    diffTables.add(new TableInfo(sourceTableName, diffColumns));
                 }
+            } else {//数据库表不存在
+                diffTables.add(new TableInfo(sourceTableName, new ArrayList<>(sourceColumnMap.values())));
             }
-            diffTables.add(source);
-        }
+        });
 
         return diffTables;
     }
 
-    /**
-     * 获取数据库表信息
-     *
-     * @return 表信息数组
-     */
+    private Map<String, Map<String, ColumnInfo>> toTableColumnMap(List<TableInfo> tables) {
+        Map<String, Map<String, ColumnInfo>> map = new HashMap<>();
+        for (TableInfo table : tables) {
+            Map<String, ColumnInfo> columnMap = new HashMap<>();
+            for (ColumnInfo column : table.columns) {
+                columnMap.put(column.name, column);
+            }
+            map.put(table.name, columnMap);
+        }
+        return map;
+    }
+
     private List<TableInfo> getDatabaseTables() {
         try (Connection conn = this.factory.newConnection()) {
             DatabaseMetaData databaseMetaData = conn.getMetaData();
@@ -117,5 +105,23 @@ public class Checker {
         } catch (SQLException e) {
             throw new SqlExCheckException(e);
         }
+    }
+
+    private List<TableInfo> getRepositoryTables() {
+        List<TableInfo> tables = new ArrayList<>();
+        for (SqlExTableInfo t : this.factory.getRepositoryClass().getAnnotationsByType(SqlExTableInfo.class)) {
+            List<ColumnInfo> columns = new ArrayList<>();
+            for (int i = 0; i < t.columnNames().length; i++) {
+                columns.add(new ColumnInfo(
+                        t.columnNames()[i],
+                        JDBCType.valueOf(t.columnTypeIds()[i]),
+                        t.columnTypeNames()[i],
+                        t.columnLengths()[i],
+                        t.columnUnsigneds()[i]
+                ));
+            }
+            tables.add(new TableInfo(t.name(), columns));
+        }
+        return tables;
     }
 }
