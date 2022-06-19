7744246fdccb25b5d3ae28d763b10fbad486e607
李雨田 <demon@danwi.me>
2022-06-19 17:17:57 +0800

    给sqlm添加代码折叠功能

---
+++ idea-plugin/src/main/kotlin/me/danwi/sqlex/idea/sqlm/SqlExMethodFoldingBuilder.kt
@@ -0,0 +1,30 @@
+package me.danwi.sqlex.idea.sqlm
+
+import com.intellij.lang.ASTNode
+import com.intellij.lang.folding.FoldingBuilderEx
+import com.intellij.lang.folding.FoldingDescriptor
+import com.intellij.openapi.editor.Document
+import com.intellij.psi.PsiElement
+import me.danwi.sqlex.idea.sqlm.psi.MethodSubtree
+
+class SqlExMethodFoldingBuilder : FoldingBuilderEx() {
+    override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
+        if (root !is SqlExMethodFile)
+            return arrayOf()
+        //取得当前sqlm文件内所有的method
+        val methodSubtrees = root.root?.methods ?: return arrayOf()
+        return methodSubtrees
+            .map { FoldingDescriptor(it, it.textRange) }
+            .toTypedArray()
+    }
+
+    override fun getPlaceholderText(node: ASTNode): String? {
+        val psiElement = node.psi
+        if (psiElement !is MethodSubtree) return null
+        return psiElement.presentation.presentableText
+    }
+
+    override fun isCollapsedByDefault(node: ASTNode): Boolean {
+        return false
+    }
+}
