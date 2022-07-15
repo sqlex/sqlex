7b7eb3ec1388205b3774eb343197d433f9a2fc6c
李雨田 <demon@danwi.me>
2022-07-15 13:59:51 +0800

    SqlExRepository注解添加继承属性

--- core/src/main/java/me/danwi/sqlex/core/annotation/SqlExRepository.java
+++ core/src/main/java/me/danwi/sqlex/core/annotation/SqlExRepository.java
@@ -2,13 +2,11 @@ package me.danwi.sqlex.core.annotation;
 
 import me.danwi.sqlex.core.RepositoryLike;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 public @interface SqlExRepository {
     Class<? extends RepositoryLike> value();
 }
