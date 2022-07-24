16a885a53c4856cfff5a668d4283bc78c9410c02
BadKid <wry10150@163.com>
2022-07-24 14:13:43 +0800

    feature(core): 增加字符串concat函数   (#129)
    * Create string.kt
    
    * 增加concat函数
    
    * 增加函数测试
    
    * 修改字符串函数的region

--- core/src/main/java/me/danwi/sqlex/core/query/expression/Expression.java
+++ core/src/main/java/me/danwi/sqlex/core/query/expression/Expression.java
@@ -2,6 +2,8 @@ package me.danwi.sqlex.core.query.expression;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public interface Expression {
     /**
@@ -167,6 +169,18 @@ public interface Expression {
                 accumulator = accumulator.or(iterator.next());
         }
         return accumulator;
+    }
+    //endregion
+    
+    //#region 字符串函数
+    static FunctionCallExpression concat(Expression... param) {
+        return func("concat", param);
+    }
+
+    static FunctionCallExpression concatWs(LiteralExpression expression, Expression... param) {
+        List<Expression> list = Arrays.stream(param).collect(Collectors.toList());
+        list.add(0, expression);
+        return new FunctionCallExpression("concat_ws", list);
     }
     //endregion
 }
