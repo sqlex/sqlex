1b3ac6e14966a2ab1e2a76baee75a94570922078
李雨田 <demon@danwi.me>
2022-07-26 22:09:23 +0800

    filter函数允许返回null

--- core-kotlin/src/main/kotlin/me/danwi/sqlex/core/query/query.kt
+++ core-kotlin/src/main/kotlin/me/danwi/sqlex/core/query/query.kt
@@ -2,4 +2,4 @@ package me.danwi.sqlex.core.query
 
 import me.danwi.sqlex.core.query.expression.Expression
 
-fun <T> WhereBuilder<T>.filter(builder: () -> Expression): T = this.where(builder.invoke())
+fun <T> WhereBuilder<T>.filter(builder: () -> Expression?): T = this.where(builder.invoke())
