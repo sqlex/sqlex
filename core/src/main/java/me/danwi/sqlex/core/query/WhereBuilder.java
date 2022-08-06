621492291e0e621039b5a7c9dc38f877d44a5d0b
王瑞玉 <wangruiyu@bjknrt.com>
2022-08-06 16:24:16 +0800

    feature(core): 修改参数构建类范型写法和构建where条件便捷方法

--- core/src/main/java/me/danwi/sqlex/core/query/WhereBuilder.java
+++ core/src/main/java/me/danwi/sqlex/core/query/WhereBuilder.java
@@ -2,6 +2,7 @@ package me.danwi.sqlex.core.query;
 
 import me.danwi.sqlex.core.query.expression.Expression;
 
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class WhereBuilder<T> {
@@ -14,14 +15,28 @@ public class WhereBuilder<T> {
      * @param exp 条件表达式
      * @return this
      */
-    public T where(Expression exp) {
+    public WhereBuilder<T> where(Expression exp) {
         if (exp == null)
-            return (T) this;
+            return this;
         if (whereCondition == null)
             whereCondition = exp;
         else
             whereCondition = whereCondition.and(exp);
-        return (T) this;
+        return this;
+    }
+
+    /**
+     * 当条件成立时，添加where条件
+     *
+     * @param condition 条件
+     * @param exp       条件表达式
+     * @return this
+     */
+    public WhereBuilder<T> where(boolean condition, Expression exp) {
+        if (condition) {
+            return this.where(exp);
+        }
+        return this;
     }
 
     /**
@@ -32,7 +47,7 @@ public class WhereBuilder<T> {
      * @deprecated 请勿使用该函数, 在以后的版本中, 会删除
      */
     @Deprecated
-    public T where(Supplier<Expression> supplier) {
+    public WhereBuilder<T> where(Supplier<Expression> supplier) {
         return where(supplier.get());
     }
 }
