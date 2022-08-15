9033ae2797e3762af17ae2303462cf0dfb5b4bdc
李雨田 <demon@danwi.me>
2022-08-15 21:41:16 +0800

    优化原生SQL执行器

--- core/src/main/java/me/danwi/sqlex/core/jdbc/RawSQLExecutor.java
+++ core/src/main/java/me/danwi/sqlex/core/jdbc/RawSQLExecutor.java
@@ -3,6 +3,7 @@ package me.danwi.sqlex.core.jdbc;
 import me.danwi.sqlex.core.ExceptionTranslator;
 import me.danwi.sqlex.core.jdbc.mapper.BasicTypeMapper;
 import me.danwi.sqlex.core.jdbc.mapper.BeanMapper;
+import me.danwi.sqlex.core.jdbc.mapper.RowMapper;
 import me.danwi.sqlex.core.transaction.Transaction;
 import me.danwi.sqlex.core.transaction.TransactionManager;
 
@@ -23,28 +24,50 @@ public class RawSQLExecutor {
 
     /**
      * 构造一个原生SQL执行器
-     * <br>
-     * 执行时将从事务管理器中获取连接
      *
+     * @param transactionManager 事务管理器
      * @param setter             参数设置器
      * @param translator         异常翻译器
-     * @param transactionManager 事务管理器
      */
-    RawSQLExecutor(ParameterSetter setter, ExceptionTranslator translator, TransactionManager transactionManager) {
+    public RawSQLExecutor(TransactionManager transactionManager, ParameterSetter setter, ExceptionTranslator translator) {
+        this.transactionManager = transactionManager;
         this.setter = setter;
         this.translator = translator;
-        this.transactionManager = transactionManager;
     }
 
     /**
-     * 执行SQL,返回影响的行数
+     * 执行SQL,返回生成键的值
+     *
+     * @param generateKeyType 生成键的Java类
+     * @param sql             SQL语句
+     * @param parameters      预处理参数
+     * @param <K>             生成键的Java类型
+     * @return 生成键的值(如果有的化, 没有则为空)
+     */
+    public <K> K insert(Class<K> generateKeyType, String sql, Object... parameters) {
+        return execute(generateKeyType, sql, Arrays.asList(parameters)).getGeneratedKey();
+    }
+
+    /**
+     * 执行SQL,返回删除的行数
      *
      * @param sql        SQL语句
      * @param parameters 预处理参数
      * @return 影响的行数
      */
-    public long execute(String sql, Object... parameters) {
-        return execute(null, sql, parameters).getAffectRows();
+    public long delete(String sql, Object... parameters) {
+        return execute(null, sql, Arrays.asList(parameters)).getAffectRows();
+    }
+
+    /**
+     * 执行SQL,返回更新的行数
+     *
+     * @param sql        SQL语句
+     * @param parameters 预处理参数
+     * @return 影响的行数
+     */
+    public long update(String sql, Object... parameters) {
+        return execute(null, sql, Arrays.asList(parameters)).getAffectRows();
     }
 
     /**
@@ -56,7 +79,7 @@ public class RawSQLExecutor {
      * @param <K>             生成键的Java类型
      * @return 执行结果
      */
-    public <K> ExecuteResult<K> execute(Class<K> generateKeyType, String sql, Object... parameters) {
+    public <K> ExecuteResult<K> execute(Class<K> generateKeyType, String sql, List<Object> parameters) {
         //尝试获取当前事务
         Transaction currentTransaction = transactionManager.getCurrentTransaction();
         Connection connection;
@@ -70,7 +93,7 @@ public class RawSQLExecutor {
         //执行SQL
         try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             //使用参数设置器设置参数
-            setter.setParameters(statement, Arrays.asList(parameters));
+            setter.setParameters(statement, parameters);
             //执行,有些驱动可能没有实现executeLargeUpdate
             long affectRows = 0;
             try {
@@ -116,7 +139,7 @@ public class RawSQLExecutor {
      * @return 结果集合
      */
     public <T> List<T> query(Class<T> beanType, String sql, Object... parameters) {
-        return query(new BeanMapper<>(beanType), sql, parameters);
+        return query(new BeanMapper<>(beanType), sql, Arrays.asList(parameters));
     }
 
     /**
@@ -128,7 +151,46 @@ public class RawSQLExecutor {
      * @param <T>        结果映射Bean类型
      * @return 结果集合
      */
-    public <T> List<T> query(BeanMapper<T> beanMapper, String sql, Object... parameters) {
+    public <T> List<T> query(BeanMapper<T> beanMapper, String sql, List<Object> parameters) {
+        return queryWithMapper(beanMapper, sql, parameters);
+    }
+
+    /**
+     * 请求单列结果
+     *
+     * @param columnType 列类型
+     * @param sql        SQL语句
+     * @param parameters 预处理参数
+     * @param <T>        列类型
+     * @return 结果集合
+     */
+    public <T> List<T> queryColumn(Class<T> columnType, String sql, Object... parameters) {
+        return queryColumn(new BasicTypeMapper<>(columnType), sql, Arrays.asList(parameters));
+    }
+
+    /**
+     * 请求单列结果
+     *
+     * @param basicMapper 基本类型映射
+     * @param sql         SQL语句
+     * @param parameters  预处理参数
+     * @param <T>         列类型
+     * @return 结果集合
+     */
+    public <T> List<T> queryColumn(BasicTypeMapper<T> basicMapper, String sql, List<Object> parameters) {
+        return queryWithMapper(basicMapper, sql, parameters);
+    }
+
+    /**
+     * 执行SQL返回结果
+     *
+     * @param rowMapper  行映射器
+     * @param sql        SQL语句
+     * @param parameters 预处理参数
+     * @param <T>        行类型
+     * @return 结果集合
+     */
+    public <T> List<T> queryWithMapper(RowMapper<T> rowMapper, String sql, List<Object> parameters) {
         //尝试获取当前事务
         Transaction currentTransaction = transactionManager.getCurrentTransaction();
         Connection connection;
@@ -142,10 +204,10 @@ public class RawSQLExecutor {
         //执行SQL
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             //使用参数设置器设置参数
-            setter.setParameters(statement, Arrays.asList(parameters));
+            setter.setParameters(statement, parameters);
             //获取返回值
             try (ResultSet rs = statement.executeQuery()) {
-                return beanMapper.fetch(rs);
+                return rowMapper.fetch(rs);
             }
         } catch (SQLException e) {
             throw translator.translate(e);
