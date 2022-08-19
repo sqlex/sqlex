61564cac44982b1c73812be2270373a12885a92d
李雨田 <demon@danwi.me>
2022-08-19 11:21:22 +0800

    优化配置解析

--- parser/src/main/kotlin/me/danwi/sqlex/parser/config/SqlExConfig.kt
+++ parser/src/main/kotlin/me/danwi/sqlex/parser/config/SqlExConfig.kt
@@ -1,8 +1,11 @@
 package me.danwi.sqlex.parser.config
 
+import com.charleskorn.kaml.MissingRequiredPropertyException
+import com.charleskorn.kaml.UnknownPropertyException
 import com.charleskorn.kaml.Yaml
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import me.danwi.sqlex.parser.exception.SqlExParserException
 
 @Serializable
 data class SqlExConfig(
@@ -12,5 +15,11 @@ data class SqlExConfig(
 )
 
 fun parseSqlExConfig(content: String): SqlExConfig {
-    return Yaml.default.decodeFromString(SqlExConfig.serializer(), content)
+    try {
+        return Yaml.default.decodeFromString(SqlExConfig.serializer(), content)
+    } catch (e: MissingRequiredPropertyException) {
+        throw SqlExParserException("配置文件错误, 缺少 ${e.path.toHumanReadableString()} 配置, (位置 ${e.line}:${e.column})")
+    } catch (e: UnknownPropertyException) {
+        throw SqlExParserException("配置文件错误, 未知的配置属性 ${e.path.toHumanReadableString()}, (位置 ${e.line}:${e.column})")
+    }
 }
