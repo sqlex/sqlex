c94491ee1663abbf475efdd30827095c346d5db5
王瑞玉 <wangruiyu@bjknrt.com>
2022-08-24 10:32:26 +0800

    新增迁移回调读取流文件

--- core/src/main/java/me/danwi/sqlex/core/migration/SQLMigrateUtil.java
+++ core/src/main/java/me/danwi/sqlex/core/migration/SQLMigrateUtil.java
@@ -4,6 +4,9 @@ import com.alibaba.druid.DbType;
 import com.alibaba.druid.sql.SQLUtils;
 import com.alibaba.druid.sql.ast.SQLStatement;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class SQLMigrateUtil {
@@ -23,5 +26,43 @@ public class SQLMigrateUtil {
                 executor.execute(statement.toString());
             }
         });
+    }
+
+    public static MigrateCallback before(int version, InputStream inputStream) {
+        String script;
+        script = readToString(inputStream);
+        if (script != null) {
+            return before(version, script);
+        }
+        return null;
+    }
+
+    public static MigrateCallback after(int version, InputStream inputStream) {
+        String script;
+        script = readToString(inputStream);
+        if (script != null) {
+            return after(version, script);
+        }
+        return null;
+    }
+
+    private static String readToString(InputStream inputStream) {
+        try (ByteArrayOutputStream result = new ByteArrayOutputStream()) {
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) != -1) {
+                result.write(buffer, 0, length);
+            }
+            return result.toString("UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
