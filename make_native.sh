6068f1350c8df82a47e155d18a0e415a37365617
李雨田 <demon@danwi.me>
2022-12-30 14:56:05 +0800

    还原make_native.sh脚本

--- make_native.sh
+++ make_native.sh
@@ -10,25 +10,15 @@ if [[ "$OSTYPE" =~ ^darwin ]];then
     else
       DIST="../../native/darwin-amd64/src/main/resources/native/darwin/amd64"
     fi
-    CGO_CFLAGS="-I$JAVA_HOME/include -I$JAVA_HOME/include/darwin"
 elif [[ "$OSTYPE" =~ ^linux ]]; then
     LIB="libsqlex.so"
     DIST="../../native/linux-amd64/src/main/resources/native/linux/amd64"
-    CGO_CFLAGS="-I$JAVA_HOME/include -I$JAVA_HOME/include/linux"
 else
   echo "不支持的操作系统,目前仅支持macos/linux"
   exit 1
 fi
 
-if [ -z $JAVA_HOME ]; then
-  echo "请设置JAVA_HOME"
-  exit 1
-fi
-
-echo "JAVA_HOME=$JAVA_HOME"
-
 echo "构建原生库"
 #构建原生库
-cd 3rdparty/mysql
-CGO_CFLAGS=$CGO_CFLAGS go build -buildmode c-shared -o $DIST/$LIB github.com/pingcap/tidb/sqlex
+cd 3rdparty/mysql && go build -buildmode c-shared -o $DIST/$LIB github.com/pingcap/tidb/sqlex
 
