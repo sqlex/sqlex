095483269fec30fcf0f177b5da69c94c3cb1aa96
李雨田 <demon@danwi.me>
2024-10-18 16:47:47 +0800

    升级gradle到8.10.2,升级idea gradle插件到2.1.0,升级kotlin到2.0.21,最低idea要求2023.1

--- idea-plugin/build.gradle.kts
+++ idea-plugin/build.gradle.kts
@@ -1,51 +1,75 @@
 plugins {
     java
     kotlin("jvm")
-    id("org.jetbrains.intellij") version "1.15.0"
+    id("org.jetbrains.intellij.platform") version "2.1.0"
+}
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
     implementation(project(":parser"))
-
     implementation("org.antlr:antlr4-intellij-adaptor:0.1")
+
+    intellijPlatform {
+        //平台依赖
+        if (project.ext.has("idea.path")) {
+            local(project.ext["idea.path"].toString())
+        } else {
+            intellijIdeaUltimate("2023.1")
+        }
+        //插件依赖
+        bundledPlugin("org.jetbrains.plugins.yaml")
+        bundledPlugin("com.intellij.java")
+        bundledPlugin("com.intellij.database")
+        bundledPlugin("org.jetbrains.kotlin")
+        bundledPlugin("com.intellij.spring")
+    }
 }
 
-// See https://github.com/JetBrains/gradle-intellij-plugin/
-intellij {
-    if (project.ext.has("idea.path")) {
-        localPath.set(project.ext["idea.path"].toString())
-    } else {
-        type.set("IU")
-        version.set("2022.1")
+intellijPlatform {
+    projectName = "sqlex"
+
+    buildSearchableOptions = false
+    instrumentCode = false
+
+    pluginConfiguration {
+        id = "me.danwi.sqlex"
+        name = "SqlEx"
+        vendor {
+            name = "SqlEx"
+            email = "demon@danwi.me"
+            url = "https://sqlex.github.io"
+        }
+        description = """
+        Adds support for the <a href="https://sqlex.github.io">sqlex</a> framework(A data access framework).
+        The following features are available:
+        <ul>
+            <li>Coding assistance(auto completion, formatting, highlighting...)</li>
+            <li>Navigation between java and sqlm, provide symbol information</li>
+            <li>Integration with Java/Kotlin/Spring</li>
+            <li>Error inspection(such as incorrect grammar)</li>
+        </ul>
+        For more information, please see <a href="https://sqlex.github.io">sqlex official site</a>.
+        """.trimIndent()
+        ideaVersion {
+            sinceBuild = "231"
+            untilBuild = "243.*"
+        }
     }
-    pluginName.set("sqlex")
-    plugins.set(
-        listOf(
-            "org.jetbrains.plugins.yaml",
-            "com.intellij.java",
-            "com.intellij.database",
-            "org.jetbrains.kotlin",
-            "com.intellij.spring"
-        )
-    )
+
+    if (ext.has("idea.token") && ext["release"] == true) {
+        publishing {
+            token = ext["idea.token"].toString()
+        }
+    }
 }
 
 tasks.runIde {
     //修改调试时候的IDEA的内存使用限制
     jvmArgs = listOf("-Xms1024m", "-Xmx2048m")
-}
-
-tasks.patchPluginXml {
-    sinceBuild.set("221")
-    untilBuild.set("242.*")
-}
-
-tasks.buildSearchableOptions {
-    enabled = false
-}
-
-if (ext.has("idea.token") && ext["release"] == true) {
-    tasks.publishPlugin {
-        token.set(ext["idea.token"].toString())
-    }
 }
